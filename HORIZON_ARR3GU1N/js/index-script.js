2e37a4ecfe0f20ded859b0d20559f73ddd2791c5
ARR3GU1N <chrisarreguin724@gmail.com>
2024-11-07 22:13:09 -0600

    Add files via upload

---
+++ HORIZON_ARR3GU1N/js/index-script.js
@@ -0,0 +1,72 @@
+const menu = document.querySelector('.menu');
+var anchoMenu = menu.clientWidth;
+
+function abrirMenu(){
+    menu.style.left = "0px";
+    menu.style.transition = '.3s';
+}
+function cerrarMenu(){
+    menu.style.left = "-" + anchoMenu + "px";
+    menu.style.transition = '.3s';
+}
+const categorias_form_menu = document.querySelector('.categorias-form-menu');
+var anchoCategorias = categorias_form_menu.clientWidth;
+
+function abrirCategoriasDeMenu(){
+    categorias_form_menu.style.left = "0px";
+    categorias_form_menu.style.transition = '.3s';
+
+    menu.style.left = "-" + anchoMenu + "px";
+    menu.style.transition = '.3s';
+}
+function cerrarCategoriasDeMenu(){
+    categorias_form_menu.style.left = "-" + anchoCategorias + "px";
+    categorias_form_menu.style.transition = '.3s';
+}
+
+const busquedaFlotante = document.querySelector('.busqueda-from-menu');
+
+function apaBusquedaFlotante(){
+    busquedaFlotante.style.opacity = '1';
+    busquedaFlotante.style.pointerEvents = 'all';
+    busquedaFlotante.style.transform = 'translateY(0)';
+    busquedaFlotante.style.transition = '.3s';
+
+    menu.style.left = "-" + anchoMenu + "px";
+    menu.style.transition = '.3s';
+}
+function desBusquedaFlotante(){
+    busquedaFlotante.style.opacity = '0';
+    busquedaFlotante.style.pointerEvents = 'none';
+    busquedaFlotante.style.transform = 'translateY(10px)';
+    busquedaFlotante.style.transition = '.3s';
+}
+
+const grande = document.querySelector('.box-carosel-ofertas');
+const punto1 = document.querySelector('.punto1');
+const punto2 = document.querySelector('.punto2');
+
+let traslado = 0;
+
+punto1.addEventListener('click', menos);
+punto2.addEventListener('click', mas);
+
+function mas(){
+
+    if(traslado != -2){
+        traslado -= 1;
+
+        grande.style.transform = `translateX(${traslado}00%)`;
+
+    }
+
+}
+function menos(){
+
+    if(traslado != 0){
+        traslado += 1;
+
+    grande.style.transform = `translateX(${traslado}00%)`;
+    }
+
+}
